c42c91689c35252581e6604a5a9f219169bd5631
D55ma <100362815+D55ma@users.noreply.github.com>
2023-02-08 07:54:09 +0300

    Update deepsource.py

--- deepsource.py
+++ deepsource.py
@@ -5,6 +5,6 @@ def append(number, number_list=[]):
     print(number_list)
     return number_list
 
-append(5) # expecting: [5], actual: [5]
-append(7) # expecting: [7], actual: [5, 7]
-append(2) # expecting: [2], actual: [5, 7, 2]
+append(5) # expecting: [5]
+append(7) # expecting: [7]
+append(2) # expecting: [2]
